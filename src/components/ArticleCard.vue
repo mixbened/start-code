4e719edb486213f8c047f51a6fea925662d4018a
Benedikt Mix <mixbened@gmail.com>
2019-10-05 23:12:15 +0200

    feat: add blogger api and cards

---
+++ src/components/ArticleCard.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="card postCard">
+        <router-link to="/article/">
+            <div class="postImage" :style="{ 'background-image': 'url(' + imageUrl + ')'}"></div>
+            <section class="section">
+                <h2 class="title is-2">{{ title }}</h2>
+                <p class="content" v-html="content"></p>
+            </section>
+        </router-link>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ArticleCard',
+  components: {
+  },
+  props: {
+      title: String,
+      content: String,
+      author: String
+    },
+    data(){
+        return {
+            imageUrl: ''
+        }
+    },
+    methods: {
+        getImage: function(){
+            // get first image from blog
+            let img = document.querySelector('.separator > a > img')
+            let source = img['attributes']['src'].value
+            this.imageUrl = source
+            img.remove()
+        }
+    },
+    mounted(){
+        this.getImage()
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.postImage {
+    background-size: cover;
+    width: 100%;
+    height: 200px;
+}
+.postCard {
+    width: 60%;
+    margin: auto;
+}
+</style>
--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -2,22 +2,37 @@
   <section>
     <Title title="Blog" subtitle="Infos rund um das Thema Programmieren lernen"/>
     <section class="section">
-        Artikel
-    </section>
-    <section class="section">
+        <ArticleCard v-for="(article,index) in posts" v-bind:key="index" v-bind:id="article.id" v-bind:title="article.title" v-bind:content="article.content" v-bind:picUrl="article.image"/>
     </section>
   </section>
 </template>
 
 <script>
 import Title from './Title.vue'
+import ArticleCard from './ArticleCard.vue'
+import axios from 'axios'
 
 export default {
   name: 'Blog',
   components: {
-      Title
+      Title,
+      ArticleCard
+  },
+  data(){
+    return {
+      posts: []
+    }
   },
   props: {
+  },
+  mounted(){
+    const id = '3080711742135149867'
+    const k = 'AIzaSyBnM-ZB84vipaMjRrN8zFo59QizrI1VgKg'
+    axios.get(`https://www.googleapis.com/blogger/v3/blogs/${id}/posts?key=${k}`)
+      .then(res => {
+        // console.log('API Response: ', res)
+        this.posts = res.data.items
+      })
   }
 }
 </script>
