d5905af319a4f8b19f7b7f625218336fcd8c9bad
Benedikt Mix <mixbened@gmail.com>
2019-10-28 17:38:45 +0100

    using api fallback

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const enforce = require('express-sslify');
 const serveStatic = require("serve-static");
+const history = require('connect-history-api-fallback');
+
+
+const port = process.env.PORT || 80;
 const path = require('path');
 const app = express();
+
+app.use(history())
 app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 80;
+
 app.listen(port);
-app.get('*', (req, res)=>{
-    res.sendFile(path.join(__dirname, 'dist')); 
-})
