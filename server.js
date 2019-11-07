fb3415c3d6186bb91f59b27ee3cd260d3313d97b
Benedikt Mix <mixbened@gmail.com>
2019-11-07 15:02:43 +0100

    fix: sitemap route blocked static serve

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ const app = express();
 app.use(require('prerender-node').set('prerenderToken', 'WJP64Rq1rCml9y2dxirN'));
 app.use(history())
 app.use(enforce.HTTPS({ trustProtoHeader: true }));
-app.get('/sitemap.xml', (req, res) => {
-    res.set('Content-Type', 'application/xml');
-    res.sendFile(path.join(__dirname, 'dist/sitemap.xml'))
-})
+// app.get('/sitemap.xml', (req, res) => {
+//     res.set('Content-Type', 'application/xml');
+//     res.sendFile(path.join(__dirname, 'dist/sitemap.xml'))
+// })
 app.use(serveStatic(path.join(__dirname, 'dist')));
 
 app.listen(port);
